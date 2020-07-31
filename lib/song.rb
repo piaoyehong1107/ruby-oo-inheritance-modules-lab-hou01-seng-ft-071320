3b527b632d4cc1ec6f0a1e04e98faf1626e569bd
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-31 23:01:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
@@ -23,7 +24,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
